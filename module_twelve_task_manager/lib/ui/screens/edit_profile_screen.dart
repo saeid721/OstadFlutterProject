706c12c33139639dc1129d1cb20409e1bc19cd42
Minhazul islam saeiD <saeid721@yahoo.com>
2023-12-20 21:10:52 +0600

    module twelve task manager update

--- module_twelve_task_manager/lib/ui/screens/edit_profile_screen.dart
+++ module_twelve_task_manager/lib/ui/screens/edit_profile_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:module_twelve_task_manager/data/models/user_model.dart';
 import 'package:module_twelve_task_manager/data/network_coller/network_coller.dart';
